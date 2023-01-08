69ca5e77a658ceaae228991c35a066688f37124c
karvok <kayvee123@gmail.com>
2023-01-08 14:47:18 -0700

    Use Nominatim API to get address coordinates

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -1,6 +1,8 @@
 const { v4: uuidv4 } = require('uuid');
 const { validationResult } = require('express-validator');
+
 const HttpError = require('../models/http-error');
+const getCoordsForAddress = require('../utils/location');
 
 let DUMMY_PLACES = [
   {
@@ -50,16 +52,23 @@ const getPlacesByUserId = (req, res, next) => {
   res.json({ places });
 };
 
-const createPlace = (req, res, next) => {
+const createPlace = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     console.log('❌ Error(s) found on createPlace:', errors);
-    throw new HttpError(
-      'Invalid inputs provided, please check your data! 🧐',
-      422
+    return next(
+      new HttpError('Invalid inputs provided, please check your data! 🧐', 422)
     );
   }
-  const { title, description, coordinates, address, creator } = req.body;
+  const { title, description, address, creator } = req.body;
+
+  let coordinates;
+  try {
+    coordinates = await getCoordsForAddress(address);
+  } catch (error) {
+    return next(error);
+  }
+
   // shorthand instead of const title = req.body.title for every property (description, coordinates, etc.)
   const createdPlace = {
     id: uuidv4(),
---
+++ utils/location.js
@@ -0,0 +1,30 @@
+const axios = require('axios');
+
+const HttpError = require('../models/http-error');
+
+const getCoordsForAddress = async (address) => {
+  const response = await axios.get(
+    `https://nominatim.openstreetmap.org/?addressdetails=1&q=${encodeURIComponent(
+      address
+    )}&format=json&limit=1`
+  );
+
+  const data = response.data;
+
+  if (!data) {
+    const error = new HttpError(
+      'Could not find location for the address provided! 😞',
+      422
+    );
+    throw error;
+  }
+
+  const coordinates = {
+    lat: data[0].lat,
+    lng: data[0].lon,
+  };
+
+  return coordinates;
+};
+
+module.exports = getCoordsForAddress;
