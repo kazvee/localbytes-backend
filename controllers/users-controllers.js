42ab6737717f443e58940542b6190534cc2faa69
karvok <kayvee123@gmail.com>
2023-01-15 18:05:35 -0700

    Add Signup New User functionality

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -2,6 +2,7 @@ const { v4: uuidv4 } = require('uuid');
 const { validationResult } = require('express-validator');
 
 const HttpError = require('../models/http-error');
+const User = require('../models/user');
 
 const DUMMY_USERS = [
   {
@@ -16,32 +17,54 @@ const getUsers = (req, res, next) => {
   res.json({ users: DUMMY_USERS });
 };
 
-const signup = (req, res, next) => {
+const signup = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    console.log('❌ Error(s) found on signup:', errors);
-    throw new HttpError(
-      'Invalid inputs provided, please check your data! 🧐',
-      422
+    return next(
+      new HttpError('Invalid inputs provided, please check your data! 🧐', 422)
     );
   }
-  const { name, email, password } = req.body;
+  const { name, email, password, places } = req.body;
 
-  const hasUser = DUMMY_USERS.find((u) => u.email === email);
-  if (hasUser) {
-    throw new HttpError('Could not create user, email already exists! 🙃', 422);
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email: email });
+  } catch (err) {
+    const error = new HttpError(
+      'Signup failed, please try again later. 🧐',
+      500
+    );
+
+    return next(error);
   }
 
-  const createdUser = {
-    id: uuidv4(),
+  if (existingUser) {
+    const error = new HttpError(
+      'Signup failed, invalid inputs provided! Please check your data. 🧐',
+      422
+    );
+    return next(error);
+  }
+
+  const createdUser = new User({
     name, // name: name
     email,
+    image: 'https://picsum.photos/200/300?random=1',
     password,
-  };
+    places,
+  });
 
-  DUMMY_USERS.push(createdUser);
+  try {
+    await createdUser.save();
+  } catch (err) {
+    const error = new HttpError(
+      '❌ Signup failed, please try again later.',
+      500
+    );
+    return next(error);
+  }
 
-  res.status(201).json({ user: createdUser });
+  res.status(201).json({ user: createdUser.toObject({ getters: true }) });
 };
 
 const login = (req, res, next) => {
