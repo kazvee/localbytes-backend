2a5900a9f73d74d759968c8e2b9b80ce9d033808
karvok <kayvee123@gmail.com>
2023-01-15 20:39:31 -0700

    Amend getPlaces, use populate method to find place

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -54,9 +54,11 @@ const getPlaceById = async (req, res, next) => {
 const getPlacesByUserId = async (req, res, next) => {
   const userId = req.params.uid;
 
-  let places;
+  // let places;
+  let userWithPlaces;
   try {
-    places = await Place.find({ creator: userId });
+    // places = await Place.find({ creator: userId });
+    userWithPlaces = await User.findById(userId).populate('places');
   } catch (err) {
     const error = new HttpError(
       'Fetching places failed, please try again later.😕',
@@ -65,7 +67,8 @@ const getPlacesByUserId = async (req, res, next) => {
     return next(error);
   }
 
-  if (!places || places.length === 0) {
+  // if (!places || places.length === 0) {
+  if (!userWithPlaces || userWithPlaces.places.length === 0) {
     return next(
       // could also use throw error instead. return here will ensure no other code executes if this response is sent.
       new HttpError('Could not find places for the provided user id. ☹️', 404)
@@ -73,7 +76,10 @@ const getPlacesByUserId = async (req, res, next) => {
   }
 
   res.json({
-    places: places.map((place) => place.toObject({ getters: true })),
+    // places: places.map((place) => place.toObject({ getters: true })),
+    places: userWithPlaces.places.map((place) =>
+      place.toObject({ getters: true })
+    ),
   });
 };
 
