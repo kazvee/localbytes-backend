4a4e10e350781ed1ebb55a556f2d6ea96955f652
karvok <kayvee123@gmail.com>
2023-01-29 19:25:02 -0700

    Add auth middleware to protect backend routes

---
+++ middleware/check-auth.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+
+const HttpError = require('../models/http-error');
+
+module.exports = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(' ')[1]; // Authorization: 'Bearer TOKEN'
+    if (!token) {
+      throw new Error('Authentication failed! 😐');
+    }
+    const decodedToken = jwt.verify(token, process.env.JWT_KEY);
+    req.userData = { userId: decodedToken.userId };
+    next();
+  } catch (err) {
+    const error = new HttpError('Authentication failed! 😐', 401);
+    return next(error);
+  }
+};
